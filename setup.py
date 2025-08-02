69ff5c85a30ee563651900aab3d6e43279597b67
DEJOKjdc <shreevishnusreekanth@gmail.com>
2025-08-03 00:52:45 +0530

    folder d

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'Generative AI Project info retrival system',
-    version= '0.0.0',
-    author= 'SHREEVISHNU',
-    author_email= 'shreevishnusreekanth@gmail.com',
-    packages= find_packages(),
-    install_requires = []
-
+    name='info-retrieval-system',
+    version='0.1.0',
+    author='SHREEVISHNU',
+    author_email='shreevishnusreekanth@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'python-dotenv',
+        'langchain-google-genai',
+        'google-generativeai',
+        'langchain',
+        'PyPDF2',
+        'faiss-cpu',
+        'streamlit'
+    ]
 )
