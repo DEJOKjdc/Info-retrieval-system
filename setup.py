513a6938c9309583779ce97793b14f4c7f2144b5
DEJOKjdc <shreevishnusreekanth@gmail.com>
2025-08-02 23:24:23 +0530

    REQ COMMIT

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project info retrival system',
+    version= '0.0.0',
+    author= 'SHREEVISHNU',
+    author_email= 'shreevishnusreekanth@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
